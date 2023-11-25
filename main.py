c3580a6f8e8d8e3d1ebcf32335dd31071a74919b
CodenificienT <61930093+codenificient@users.noreply.github.com>
2023-11-25 02:21:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from FastAPI!"}
-
+    return {"health_check": "OK", "model_version": "0.1.3"}
+    
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
