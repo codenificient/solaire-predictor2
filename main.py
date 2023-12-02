dbd7eebe58ed5cda82acdfa78f78c4b8061fea12
CodenificienT <codenificient@tutanota.com>
2023-12-02 14:55:25 -0500

    add electrification rate prediction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from model.model import predict_demand_pipeline, predict_gdp_growth, predict_population
+from model.model import predict_electricity_usage, predict_gdp_growth, predict_population, predict_electrification
 from model.model import __version__ as model_version
 import uvicorn
 
@@ -9,6 +9,7 @@ app = FastAPI(title="Solaire Predictor API")
 class TextInput(BaseModel):
     country_code: str
     year: str
+    use_linear: bool
 
 class PredictionOut(BaseModel):
     predicted_value: float
@@ -17,20 +18,25 @@ class PredictionOut(BaseModel):
 def home():
     return {"health_check": "OK", "model_version": model_version}
 
-@app.post("/predict-demand", response_model=PredictionOut)
+@app.post("/predict-electricity-demand", response_model=PredictionOut)
 def predict(payload: TextInput):
-    energy_demand = predict_demand_pipeline(payload.country_code, payload.year)
+    energy_demand = predict_electricity_usage(payload.country_code, payload.year, payload.use_linear)
     return {"predicted_value": energy_demand}
 
 @app.post("/predict-gdp-growth", response_model=PredictionOut)
 def predict(payload: TextInput):
-    gdp_growth = predict_gdp_growth(payload.country_code, payload.year)
+    gdp_growth = predict_gdp_growth(payload.country_code, payload.year, payload.use_linear)
     return {"predicted_value": gdp_growth}
 
 @app.post("/predict-population", response_model=PredictionOut)
 def predict(payload: TextInput):
-    population = predict_population(payload.country_code, payload.year)
+    population = predict_population(payload.country_code, payload.year, payload.use_linear)
     return {"predicted_value": population}
+
+@app.post("/predict-electrification", response_model=PredictionOut)
+def predict(payload: TextInput):
+    rate = predict_electrification(payload.country_code, payload.year, payload.use_linear)
+    return {"predicted_value": rate}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- model/model.py
+++ model/model.py
@@ -10,8 +10,8 @@ def load_model(modelname):
     with open(modelname, "rb") as saved_model:
         return pickle.load(saved_model)
 
-def predict_demand_pipeline(country_code, year):
-    modelname = f"{BASE_DIR}/linear_electric_usage-{__version__}.pkl"
+def predict_electricity_usage(country_code, year, use_linear=False):
+    modelname = f"{BASE_DIR}/linear_electricity_usage-{__version__}.pkl" if use_linear else f"{BASE_DIR}/xgb_electricity_usage-{__version__}.pkl"
     linear_model = load_model(modelname)
 
     # Load the encoding mapping file
@@ -37,8 +37,9 @@ def predict_demand_pipeline(country_code, year):
     print(f'Predicted Energy Usage for {country_name} in {year}: {round(adjusted_predicted_value_rmse[0], 2)}')
     return round(adjusted_predicted_value_rmse[0], 2)
 
-def predict_gdp_growth(country_code, year):
-    modelname = f"{BASE_DIR}/xgb_gdp_growth-{__version__}.pkl"
+def predict_gdp_growth(country_code, year, use_linear=False):
+    modeltype = "LINEAR MODEL" if use_linear else "XGB MODEL"
+    modelname = f"{BASE_DIR}/linear_gdp-growth-{__version__}.pkl" if use_linear else f"{BASE_DIR}/xgb_gdp_growth-{__version__}.pkl"
     xgb_model = load_model(modelname)
     # Load the encoding mapping file
     with open(f"{BASE_DIR}/encoding_mapping.csv", "rb") as csv_encoding:
@@ -60,11 +61,12 @@ def predict_gdp_growth(country_code, year):
     # Use the trained model to predict the value for the user-provided country and year
     predicted_value = xgb_model.predict(country_code_encoded)
 
-    print(f'Predicted GDP Growth for {country_name} in {year}: {round(predicted_value[0], 2)}')
+    print(f'[{modeltype}] Predicted GDP Growth for {country_name} in {year}: {round(predicted_value[0], 2)}')
     return round(predicted_value[0], 2)
 
-def predict_population(country_code, year):
-    modelname = f"{BASE_DIR}/xgb_population-{__version__}.pkl"
+def predict_population(country_code, year, use_linear=False):
+    modeltype = "LINEAR MODEL" if use_linear else "XGB MODEL"
+    modelname = f"{BASE_DIR}/linear_population-{__version__}.pkl" if use_linear else f"{BASE_DIR}/xgb_population-{__version__}.pkl"
     xgb_model = load_model(modelname)
     # Load the encoding mapping file
     with open(f"{BASE_DIR}/encoding_mapping.csv", "rb") as csv_encoding:
@@ -86,5 +88,33 @@ def predict_population(country_code, year):
     # Use the trained model to predict the value for the user-provided country and year
     predicted_value = xgb_model.predict(country_code_encoded)
 
-    print(f'Predicted Population for {country_name} in {year}: {round(predicted_value[0])}')
+    print(f'[{modeltype}] Predicted Population for {country_name} in {year}: {round(predicted_value[0])}')
     return round(predicted_value[0])
+
+
+def predict_electrification(country_code, year, use_linear=False):
+    modeltype = "LINEAR MODEL" if use_linear else "XGB MODEL"
+    modelname = f"{BASE_DIR}/linear_electrification_rate-{__version__}.pkl" if use_linear else f"{BASE_DIR}/xgb_electrification_rate-{__version__}.pkl"
+    xgb_model = load_model(modelname)
+    # Load the encoding mapping file
+    with open(f"{BASE_DIR}/encoding_mapping.csv", "rb") as csv_encoding:
+        encoding_mapping = pd.read_csv(csv_encoding, encoding="utf-8")
+
+        # Load the encoding mapping file
+    with open(f"{BASE_DIR}/countries_mapping.csv", "rb") as countries_encoding:
+        countries_mapping = pd.read_csv(countries_encoding, encoding="utf-8")
+
+    # Assuming 'user_country_code' is the user-provided country code
+    country_code_encoded = encoding_mapping[encoding_mapping['Country Code'] == country_code.upper()].iloc[:, 1:]
+
+    # Get the country name for the provided country code
+    country_name = countries_mapping.loc[countries_mapping['Country Code'] == country_code.upper(), 'Country Name'].iloc[0]
+
+    # Add the 'Year' column to the front of the dataframe
+    country_code_encoded.insert(0, 'Year', int(year))
+
+    # Use the trained model to predict the value for the user-provided country and year
+    predicted_value = xgb_model.predict(country_code_encoded)
+
+    print(f'[{modeltype}] Predicted Electrification Rate for {country_name} in {year}: {round(predicted_value[0], 2)}%')
+    return round(predicted_value[0], 2)
